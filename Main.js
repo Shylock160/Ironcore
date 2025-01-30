425f111499fe501553569a0b26376b38e1f7a99a
Davidson Daniel <shylock160@gmail.com>
2025-01-30 15:11:47 -0800

    started working on service section

---
+++ Main.js
@@ -0,0 +1,15 @@
+let x = document.querySelector("#logIn");
+let y = document.querySelector("#signUp");
+let z = document.querySelector("#btn");
+
+function signup(){
+    x.style.left = "-400px"
+    y.style.left = "5px"
+    z.style.left = "110px"
+}
+
+function login(){
+    x.style.left = "5px"
+    y.style.left = "450px"
+    z.style.left = "0"
+}
