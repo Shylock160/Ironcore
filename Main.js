b6922edc7e381465d43b4df8b58963e61047385a
Davidson Daniel <shylock160@gmail.com>
2025-02-06 20:10:59 -0800

    I am done with the website.

--- Main.js
+++ Main.js
@@ -13,3 +13,19 @@ function login(){
     y.style.left = "450px"
     z.style.left = "0"
 }
+
+// login and sign up alert
+
+let logInBtn = document.querySelector("#logInBtn");
+let signUpBtn = document.querySelector("#signUpBtn");
+
+function handleLogInBtn (){
+    alert("Login successful!");
+}
+
+function handleSignUpBtn (){
+    alert("Sign up successful!");
+}
+
+logInBtn.addEventListener("click", handleLogInBtn);
+signUpBtn.addEventListener("click", handleSignUpBtn);
